5144ad9e3da581c108e08a2a3b4c5abd4fbec9ed
Artur Mineev <arturmineev9@mail.ru>
2026-05-17 23:09:01 +0300

    added ktor network client

---
+++ Saved/shared/src/commonMain/kotlin/org/example/saved/data/network/NetworkClient.kt
@@ -0,0 +1,28 @@
+package org.example.saved.data.network
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+fun createHttpClient(): HttpClient {
+    return HttpClient {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+
+        defaultRequest {
+            url("http://127.0.0.1:8080/")
+            contentType(ContentType.Application.Json)
+        }
+
+        // TODO: Auth (Авторизация и Refresh Token)
+    }
+}
